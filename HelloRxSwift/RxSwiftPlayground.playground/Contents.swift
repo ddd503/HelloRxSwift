a41066bdd13ded17cdc904f2774a828e02fd03fd
Dai Kawahara <skbsporta7@gmail.com>
2019-10-24 21:23:10 +0900

    learn subscribe

--- HelloRxSwift/RxSwiftPlayground.playground/Contents.swift
+++ HelloRxSwift/RxSwiftPlayground.playground/Contents.swift
@@ -2,3 +2,30 @@ import UIKit
 import RxSwift
 
 _ = Observable.from([1, 2, 3, 4, 5])
+
+let observable = Observable.just(1)
+
+let observable2 = Observable.of(1, 2, 3)
+
+let observable3 = Observable.of([1, 2, 3])
+
+let observable4 = Observable.from([1, 2, 3, 4, 5])
+
+// 順番に出力
+observable4.subscribe { event in
+    if let element = event.element {
+        print(element)
+    }
+}
+
+// 配列ごと出力
+observable3.subscribe { event in
+    if let element = event.element {
+        print(element)
+    }
+}
+
+// 非オプショナルで返り値がくる
+observable4.subscribe(onNext: { element in
+    print(element)
+})
